6dd6a128388f0351ff5e72e589fb4073ac4115b4
Mitch <mitchjoelklein@hotmail.com>
2024-10-16 15:25:18 +1100

    Inquirer questions added

--- index.js
+++ index.js
@@ -7,20 +7,63 @@ import fs from "fs";
 const questions = [
   {
     type: "input",
-    name: "Sample Question 1",
-    message: colors.magenta("Question text here"),
+    name: "Project Title",
+    message: colors.magenta("Project title: "),
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: colors.magenta("Project description:"),
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: colors.magenta("Project installation instructions: "),
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: colors.magenta("Project usage information: "),
+  },
+  {
+    type: "input",
+    name: "Contributing",
+    message: colors.magenta("Project contribution guidelines: "),
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: colors.magenta("Project test instructions: "),
   },
   {
     type: "checkbox",
-    message: colors.brightMagenta("Question text here"),
-    name: "Checkbox list",
-    choices: [],
+    name: "License",
+    message: colors.brightMagenta("Select a license"),
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      "BSD 2-Clause 'Simplified' License",
+      "BSD 3-Clause 'New' or 'Revised' License",
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public License v2.1",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+    ],
   },
   {
-    type: "list",
-    message: colors.brightMagenta("Question text"),
-    name: "List question",
-    choices: [],
+    type: "input",
+    name: "Username",
+    message: colors.magenta("Github username: "),
+  },
+  {
+    type: "input",
+    name: "Email",
+    message: colors.magenta("Email address: "),
   },
 ];
 
